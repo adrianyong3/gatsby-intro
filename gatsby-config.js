3760d0c6df0db770af35ba6f65439c275cb2d4ab
Adrian Yong <11637545+adrianyong3@users.noreply.github.com>
2019-07-26 21:43:08 +1000

    Add gatsby webpack analyzer plugin and built the site

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,17 @@ module.exports = {
     title: 'Frontend Masters Gatsby Workshp',
     description: 'A site we built during FEM workshop',
   },
-  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+      options: {
+        production: true,
+        diable: !process.env.ANALYZE_BUNDLE_SIZE,
+        generateStatsFile: true,
+        analyzerMode: 'static',
+      },
+    },
+  ],
 };
